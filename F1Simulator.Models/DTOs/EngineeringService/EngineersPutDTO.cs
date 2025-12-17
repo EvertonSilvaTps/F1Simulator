90b33f17ac4344bac44247c87f01a314d0104de1
Everton Silva <everton.tps@outlook.com>
2025-12-17 14:33:27 -0300

    Revert "Branch everton"

--- F1Simulator.Models/DTOs/EngineeringService/EngineersPutDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace F1Simulator.Models.DTOs.EngineeringService
-{
-    public class EngineersPutDTO
-    {
-        public string? EngineerCaId { get; init; }
-        public string? EngineerCpId { get; init; }
-    }
-}
